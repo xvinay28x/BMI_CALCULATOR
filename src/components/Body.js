7dd6736867647426827bf1804ac9fe3e53a5fe94
xvinay28x <xvinay28x@gmail.com>
2023-09-30 01:54:52 +0530

    initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Body.css'
+
+export default function body() {
+    return (
+        <div>
+            <img id="male" src="man.png"></img>
+            <img id="female" src="woman.png"></img>
+        </div>
+    )
+}
---
+++ src/components/Nevbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nevbar.css'
+
+export default function Nevbar() {
+    return (
+        <div className="header">
+            <img id="gif" src="1.gif"></img>
+            <h1 id="heading1">BMI</h1>
+            <p id="heading2">CALCULATOR</p>
+        </div>
+    )
+}
