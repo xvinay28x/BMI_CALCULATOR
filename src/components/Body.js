06a5349d79f857cb0ada73541113c83a2a26eb45
xvinay28x <xvinay28x@gmail.com>
2023-09-30 16:09:17 +0530

    aading somw stuff

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,9 +5,27 @@ export default function body() {
     return (
         <div className="body">
             <h1 id="text">Choose Your Sex</h1>
-            <img id="male" src="man.png"/>
-            <img id="female" src="woman.png"/>
-            
+            <div className="img_cont">
+                <div className="male_cont">
+                    <img id="male" src="man.png" />
+                    <input id="male_inp" type="radio" name="sex" value="male"></input>
+                </div>
+                <div className="female_cont">
+                    <img id="female" src="woman.png" />
+                    <input id="female_inp" type="radio" name="sex" value="female"></input>
+                </div>
+            </div>
+            <div class="height_cont">
+                <input id="height" type="number"></input>
+                <h1 id="height_label">Height <p id="cm">(cm)</p></h1>
+            </div>
+            <div class="weight_cont">
+                <input id="weight" type="number"></input>
+                <h1 id="weight_label">Weight <p id="kg">(kg)</p></h1>
+            </div>
+            <div className="button_cont">
+                <button id="calculate">Calculate</button>
+            </div>
         </div>
     )
 }
